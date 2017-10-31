d4fd5cae532e7cd737ef0c52df003826844bdee7
Avisible <alekseev.alexander.83@gmail.com>
2017-10-31 23:22:32 +0300

    task3_1_2 corrected

--- task3_1_2.java
+++ task3_1_2.java
@@ -6,22 +6,20 @@ public class task3_2 {
 		// TODO Auto-generated method stub
 
 		int[] initArray = { 2, 0, 3, 1, 0 }; // создание массива целочисленной последовательности с нулевыми элементами
-		int count = 0; // промежуточная переменная для хранения количества нулей найденных в массиве
-						// initArray
-		int size = zeroCount(initArray, count);
-		// System.out.println("новый массив будет содержать " + size + " элемента");
-		// это нужно для дебага
-		int[] withZeroArray = new int[size];
+
+		int size = zeroCount(initArray);
+
+		int[] withZeroArray;
 		withZeroArray = fillInZeroArray(initArray, size);
 
 		System.out.print("\nВывод элементов массива withZeroArray: ");
 		for (int i = 0; i < withZeroArray.length; i++) {
 			System.out.print(withZeroArray[i] + " ");
 		}
-
 	}
 
-	public static int zeroCount(int[] initArray, int count) { // метод для поиска нулей в массиве initArray
+	public static int zeroCount(int[] initArray) { // метод для поиска нулей в массиве initArray
+		int count = 0;
 		for (int i = 0; i < initArray.length; i++) {
 			if (initArray[i] == 0) {
 				count = count + 1;
@@ -31,18 +29,17 @@ public class task3_2 {
 	}
 
 	public static int[] fillInZeroArray(int[] initArray, int size) {
-		int[] result = new int[size];
+		int[] withZeroArray = new int[size];
+		int k = 0;
 		for (int i = 0; i < initArray.length; i++) {
 			if (initArray[i] == 0) {
-				int j = 0;
-				result[j] = i;
-				System.out.println(result[j]);
-				j++;
+				withZeroArray[k] = i;
+				k++;
 			}
 
 		}
 
-		return result;
+		return withZeroArray;
 
 	}
 
